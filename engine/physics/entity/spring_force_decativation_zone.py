fed999c6d5328bac8f406a87efff801874285ec3
akashkesani <akash.kesani1@gmail.com>
2023-01-23 10:05:36 -0500

    Adding components to game engine

---
+++ engine/physics/entity/spring_force_decativation_zone.py
@@ -0,0 +1,13 @@
+
+
+class SpringForceDeactivationZone:
+
+
+    def __init__(self, position_x_, position_y_, height_, width_):
+        self.name = 'Spring Force Deactivation Zone'
+        self.position_x = position_x_
+        self.position_y = position_y_
+        self.height = height_
+        self.width = width_
+        self.verbose = True
+        
